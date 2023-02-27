841d29d0e1d4daae9ad18d67b2405a44079961c1
Pooja Hegde <poojahegde05@gmail.com>
2023-02-26 16:25:51 -0800

    Task2: Adding instant search

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -8,10 +8,12 @@ describe('When: Use the search feature', () => {
 
     cy.get('form').submit();
 
-    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 0);
   });
 
-  xit('Then: I should see search results as I am typing', () => {
-    // TODO: Implement this test!
+  it('Then: I should see search results as I am typing', () => {
+    cy.get('input[type="search"]').type('java');
+
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 0);
   });
 });
--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,30 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
   clearSearch,
   getAllBooks,
-  searchBooks
+  searchBooks,
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.scss']
+  styleUrls: ['./book-search.component.scss'],
 })
-
-export class BookSearchComponent {
+export class BookSearchComponent implements OnInit, OnDestroy {
   books$ = this.store.select(getAllBooks);
+  subscription = new Subscription();
   searchForm = this.fb.group({
-    term: ''
+    term: '',
   });
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
-  ) {}
+    private readonly fb: FormBuilder,
+  ) {
+   }
+
+  ngOnInit(): void {
+
+    this.searchForm.get('term').valueChanges
+    .pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$.asObservable())
+    ).subscribe(() => this.searchBooks());
+  }
 
   addBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
@@ -32,14 +47,18 @@ export class BookSearchComponent {
 
   searchExample() {
     this.searchForm.controls.term.setValue('javascript');
-    this.searchBooks();
   }
 
   searchBooks() {
     if (this.searchForm.value.term) {
-      this.store.dispatch(searchBooks({ term: this.searchForm.value.term }));
+      this.store.dispatch(searchBooks({ term: this.searchForm.value.term}));
     } else {
       this.store.dispatch(clearSearch());
     }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 }
